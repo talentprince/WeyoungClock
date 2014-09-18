554065d937dfcf401ce71a31a5236d0161095dec
chenchen <talent_prince@163.com>
2014-09-18 16:01:04 +0800

    build simple widget framework for project

---
+++ src/org/weyoung/clock/WeyoungClock.java
@@ -0,0 +1,20 @@
+package org.weyoung.clock;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class WeyoungClock extends AppWidgetProvider {
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		// TODO Auto-generated method stub
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+	}
+
+	@Override
+	public void onDeleted(Context context, int[] appWidgetIds) {
+		// TODO Auto-generated method stub
+		super.onDeleted(context, appWidgetIds);
+	}
+}
